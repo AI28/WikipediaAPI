6875d87d0d6eb96de856550fd0308448db4927d2
Alex Ichim <alexandru.ichim07@gmail.com>
2020-12-09 17:48:39 +0200

    Scrapping; almost done. Small bug fixes and neighbouring countries left

--- Scraper/DocumentScrapper.py
+++ Scraper/DocumentScrapper.py
@@ -1,3 +1,5 @@
+import re
+
 from bs4 import BeautifulSoup
 
 from Scraper.DocumentGetter import DocumentGetter
@@ -29,7 +31,7 @@ class ListOfCountriesScrapper(DocumentScrapper):
     def __init__(self, url):
         super().__init__(url)
 
-    def scrap_document(self):
+    def __scrap_document(self):
         aux = self._parser.find_all('a')
         self.__elements = [item.attrs["href"] for item in aux
                            if "title" in item.attrs
@@ -38,7 +40,7 @@ class ListOfCountriesScrapper(DocumentScrapper):
 
     def get_elements(self):
         if self.__elements is None:
-            raise Exception("Exception. Unitialized elements list. Please call scrapDocument first.")
+            self.__scrap_document()
 
         return self.__elements
 
@@ -48,14 +50,123 @@ class CountryDataScrapper(DocumentScrapper):
 
     def __init__(self, url):
         super().__init__(url)
+        self.scrap_document()
 
     def scrap_document(self):
 
         self.__country_card = self._parser.find('table', class_="infobox geography vcard")
 
+    def get_country_name(self):
+
+        """return super()._document_getter.get_url().split('/')[-1]"""
+        if self.__country_card is None:
+            self.scrap_document()
+
+        aux = self.__country_card.find(class_="country-name")
+
+        if aux is None:
+            return aux
+
+        return self.__country_card.find(class_="country-name").text
+
+    def get_country_capital(self):
+
+        if self.__country_card is None:
+            return None
+
+        t = self.get_country_card().find(text="Capital")
+
+        if t is None:
+            return None
+
+        return t.find_parent().find_next_sibling().find("a").text
+
+    def get_country_population(self):
+
+        if self.__country_card is None:
+            return 0
+
+        t = self.get_country_card().find(text="Population")
+
+        if t is None:
+            return 0
+
+        milions_re = re.compile("[1-9]\d{0,2}(,\d{3})*")
+
+        aux = t.find_all_next(text=milions_re)[1]
+        result = int(re.search(milions_re, str(aux.string))[0].replace(",", ''))
+
+        if result < 100:
+            return result * 1000000
+
+        return result
+
+    def get_country_language(self):
+
+        if self.__country_card is None:
+            self.scrap_document()
+
+        t = set(self.get_country_card().find_all(title=re.compile("language$")))
+
+        return list(map(lambda x: x.text, t))
+
+    def get_country_government(self):
+        if self.__country_card is None:
+            self.scrap_document()
+
+        t = self.__country_card.find(text="Government")
+
+        if t is None:
+            return None
+
+        t1 = t.find_parent().find_parent().find_next_sibling("td")
+
+        if t1 is None:
+            return None
+
+        return list(map(lambda x: x.text, t1.find_all("a")))
+
+    def get_country_area(self):
+
+        if self.__country_card is None:
+            self.scrap_document()
+
+        milions_re = re.compile("[1-9]\d{0,2}((,|\.)\d{3})*")
+
+        area_element = self.__country_card.find(
+            text=re.compile("Area")).find_parent().find_parent().find_parent().find_next_sibling()
+
+        if area_element is None:
+            return -1
+        elif area_element.find("td") is None:
+            return -1
+
+        match =  re.match(milions_re,area_element.find("td").text)
+
+        if match is None:
+            return -1
+        elif match.group(0) is None:
+            return -1
+
+        return int(match.group(0).replace(",",""))
+    def get_neighbours(self):
+        if self.__country_card is None:
+            self.scrap_document()
+
+        return self.__country_card.find_all(text = re.compile("surrounded|borders"))
+
+    def get_time_zone(self):
+
+        if self.__country_card is None:
+            self.scrap_document()
+
+        time_zone_element = self.__country_card.find_all(text=re.compile("(UTC|GMT)"))[0].find_parent().find_parent()
+
+        return time_zone_element.text
+
     def get_country_card(self):
 
         if self.__country_card is None:
-           self.scrap_document()
+            self.scrap_document()
 
         return self.__country_card
--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import json
+import re
+
 from Scraper.DocumentScrapper import ListOfCountriesScrapper, CountryDataScrapper
 
 
@@ -18,31 +21,50 @@ def print_hi():
     return test1
 
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    docs = print_hi()
-    docs.scrap_document()
-    test = docs.get_elements()
-
-    test = []
     try:
+        list_of_countries = ListOfCountriesScrapper("https://en.wikipedia.org/wiki/List_of_sovereign_states")
         wiki = "https://en.wikipedia.org"
-        for i in docs.get_elements():
+        for j in list_of_countries.get_elements():
 
-
-            aux = CountryDataScrapper(wiki + i)
+            aux = CountryDataScrapper(wiki + j)
 
             if aux.get_country_card() is None:
                 continue
 
-            test.append(aux)
-            for j in aux.get_country_card().children:
-                print(j)
+            country_name = aux.get_country_name()
+            if country_name is None:
+                continue
 
+            country_area = aux.get_country_area()
+            country_capital = aux.get_country_capital()
+            country_population = aux.get_country_population()
+            country_government = aux.get_country_government()
+            country_language = aux.get_country_language()
+            country_time_zone = aux.get_time_zone()
+            country_population_density = None
+
+            if country_population is None or country_area is None:
+                country_population_density = 0
+
+            country_population_density = country_population // country_area
+
+            print(country_area)
+            print(country_name)
+            print(country_population)
+            print(country_government)
+            print(country_language)
+            print(country_time_zone)
+            print(country_population_density)
+
+            t2 = {"name": country_name, "area": country_area, "population": country_population,
+                  "government": country_government, "languages": country_language, "timezone": country_time_zone,
+                  "density": country_population_density}
+
+            print(json.dumps(t2))
+            print(aux.get_neighbours())
 
     except Exception as e:
         print(str(e))
-
-
